027ceaf2a592f9f78f3b29a393d6f977b30c2f5a
kukushka <rezniknikita57@gmail.com>
2024-09-07 00:43:03 +0300

    Выполненное задание

---
+++ lesson-1/lesson-2.py
@@ -0,0 +1,18 @@
+# apples = 41 
+# boxes = 3 
+# full_boxes = 41 // 3 
+# print("Всего ящиков:", full_boxes)
+# tone_left = apples % boxes
+# print("Осталось тон:", tone_left)
+
+num = int(input("Введите число:"))
+house_number = num % 10
+apartment_number = num // 10
+print("Номер дома:", house_number)
+print("Номер кваритры:", apartment_number)
+
+
+
+
+
+
