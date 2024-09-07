c8e67b05a33b12216a027ed8c222e6df663d2b00
Аирнет Поддержка <rezniknikita57@gmail.com>
2024-09-07 20:41:13 +0300

    new lesson-4 file

---
+++ lesson-1/lesson-4.py
@@ -0,0 +1,31 @@
+# a = 6
+
+# b = 2
+
+# c = 0
+
+# if b < a:
+
+#     c = a * b
+
+# print(c)
+
+
+# sum = int(input("Введите состояние счёта:"))
+
+# if sum >= 75000: 
+#     sum -= 75000
+#     print("Курс успешно приобретён!") 
+#     print("Баланс на карте:", sum)
+
+# print("Хорошего дня!")
+
+sun = int(input("Какое число я загадал?"))
+father = 5
+if father == sun:
+    print("Угадал")
+
+if father != sun:
+    print("Не угадал")
+
+print("Конец игры")
