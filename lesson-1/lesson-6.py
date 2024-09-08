ea0fa3ef8fae8105ae9fc2672b8ef96f0c808355
kukushka <rezniknikita57@gmail.com>
2024-09-09 02:31:03 +0300

    new file lesson-6

---
+++ lesson-1/lesson-6.py
@@ -0,0 +1,60 @@
+# income = int(input("Ваша зарплата: "))
+
+# if income < 0:
+#     print("Ошибка: доход не может быть отрицательным")
+# elif income == 0:
+#     print("Вы не зарабатываете")
+# elif income < 10000:
+#     percent =  income * 13 / 100
+#     print("Процент налога (13%):", percent)
+# elif income < 50000:
+#     percent = income * 20 / 100 
+#     print("Процент налога (20%):", percent)
+# else:
+#     percent = income * 30 / 100 
+#     print("Процент налога (30%):", percent)
+
+
+
+
+# coin_1 = int(input("Введите вес 1й монетки:"))
+# coin_2 = int(input("Введите вес 2й монетки:"))
+# coin_3 = int(input("Введите вес 3й монетки:"))
+
+# if coin_1 == coin_2 :
+#     print("Третья легче")
+# elif coin_1 == coin_3:
+#     print("Вторая легче")
+# else: 
+#     print("Первая легче")
+
+
+
+year = int(input("Год выпуска: "))
+speed = int(input("Количество скоростей: "))
+
+if year > 2018 and speed > 24:
+    print("Подходит")
+else:
+    print("Не подходит")
+
+
+
+# scores = int(input("Сколько баллов набрал? "))
+# medal = int(input("Есть медаль? "))
+
+# if scores < 280 or medal == 0:
+#     print("К сожалению, ты не прошёл в наш университет.")
+# else:
+#     print("Поздравляем! Ты поступил!")
+
+
+
+
+# temperature = int(input("Введите температуру: "))
+
+# if temperature <= 0 or temperature >= 100:
+#     print("Опасная температура")
+# else:
+#     print("Температура в пределах нормы")
+
