9e88436b5da3255da3724ad45980e407687ca928
kukushka <rezniknikita57@gmail.com>
2024-09-12 02:15:49 +0300

    new task

---
+++ lesson-1/lesson-9.py
@@ -0,0 +1,43 @@
+# tusk_1
+
+# number = int(input("Введите число: "))
+# n = 0
+# number_degree = 0
+# while n < number:
+#     number_degree += 1
+#     n += 1
+#     summ = number_degree ** 3
+#     print(number_degree, "** 3 =", summ)
+    
+
+# tusk_2
+
+# name = input("Имя должника: ")
+# summ = int(input("Сколько должен: "))
+# print(name + ", ваша задолженность составляет", summ, "рублей.")
+
+# while True:
+#     money = int(input("Сколько рублей вы внесёте прямо сейчас, чтобы её погасить? "))
+#     if money >= summ:
+#         print("Отлично,", name + "! Вы погасили долг. Спасибо!")
+#         break
+#     print("Маловато,", name + ". Давайте ещё раз.")
+
+
+
+# tusk_3
+
+# number = int(input("Введите число: "))
+# if number == 0:
+#     total_numbers = 1 
+# else:
+#     total_numbers = 0
+#     while number > 0:
+#         number //= 10
+#         total_numbers += 1
+
+# print(total_numbers)
+
+
+
+# tusk_4
