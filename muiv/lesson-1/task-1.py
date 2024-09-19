dc92e6b7d3de40a198a31b3ff9dc26e4c39569e6
kukushka <rezniknikita57@gmail.com>
2024-09-19 23:17:52 +0300

    task

--- muiv/lesson-1/task-1.py
+++ muiv/lesson-1/task-1.py
@@ -1,5 +1,17 @@
-import sys
-first_name = sys.argv[1]  
-last_name = sys.argv[2]
+# import sys 
+# weight = float(sys.argv[1])
+# speed = float(sys.argv[2])
 
-print("Hello", first_name, last_name)
+
+# # weight = int(input("Вес: "))
+# # speed = int(input("Скорость: "))
+
+
+
+# kinetic_energy = weight * speed ** 2 / 2
+# print(kinetic_energy)
+
+first_name = "Nikita"
+last_name = "Reznik"
+print(last_name, first_name[0], end=".\n")
+
